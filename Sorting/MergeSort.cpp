bf0c66353a01f408b832f579677ee2f8167787e9
Sourav <ksourav662@gmail.com>
2024-09-15 15:28:52 +0530

    mergeSort

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left++]);
+        }
+        else
+        {
+            temp.push_back(arr[right++]);
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left++]);
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[right++]);
+    }
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+void printArr(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the array" << endl;
+    cin >> size;
+    vector<int> arr(size);
+    cout << "Enter" << size << " element of the array" << endl;
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    mergeSort(arr, 0, size - 1);
+    printArr(arr, size);
+
+    return 0;
+}
