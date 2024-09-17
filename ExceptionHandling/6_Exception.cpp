9e8526a1e14646b0e485b3b7ec4b482b28b8eff9
Sourav <ksourav662@gmail.com>
2024-09-17 11:34:42 +0530

    Exception with multiple catch block

---
+++ ExceptionHandling/6_Exception.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void testException(int num)
+{
+    try
+    {
+        if (num == 0)
+        {
+            throw "Zero is not allowed!";
+        }
+        else if (num < 0)
+        {
+            throw num;
+        }
+        else if (num > 10)
+        {
+            throw out_of_range("Number too large!");
+        }
+        else
+        {
+            cout << "Number is valid: " << num << endl;
+        }
+    }
+    catch (const char *e)
+    {
+        cout << "Exception: " << e << endl;
+    }
+    catch (int n)
+    {
+        cout << "Exception: Invalid no" << n << endl;
+    }
+    catch (const out_of_range &e)
+    {
+        cout << "Exception: Number out of range" << e.what() << endl;
+    }
+}
+
+int main()
+{
+    testException(0);
+    testException(-5);
+    testException(15);
+    testException(5);
+
+    return 0;
+}
