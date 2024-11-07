60d96080f939177bd06bc7bdb6a0d0f8c99eae63
Sourav <ksourav662@gmail.com>
2024-11-07 23:57:14 +0530

    Basic patterns

---
+++ Pattern/patterns.cpp
@@ -0,0 +1,268 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= i)
+            {
+                cout << " * ";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern3(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j >= (c + 1) - i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern4(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= (r + 1) - i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern5(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j >= i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern6(int r = 5, int c = 9)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j >= 6 - i && j <= 4 + i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern7(int r = 5, int c = 9)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j >= i && j <= 10 - i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern8(int r = 5, int c = 9)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j >= 6 - i && j <= 4 + i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j >= i && j <= 10 - i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern9(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= (r + 1) - i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern10(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= i)
+            {
+                cout << j;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern11(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= i)
+            {
+                cout << i;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern12(int r, int c)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (j <= (r + 1) - i)
+            {
+                cout << j;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int row, col;
+    cout << "Enter How many Rows:" << endl;
+    cin >> row;
+    cout << "Enter How many Cols:" << endl;
+    cin >> col;
+    cout << endl;
+    pattern12(row, col);
+    return 0;
+    // pattern6(); default parameter
+}
